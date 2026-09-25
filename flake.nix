057b41ccb903ccb685ef345b1560c65508cf6f33
cherrblyria <cherrblyria@gmail.com>
2026-09-25 10:42:51 +0700

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "cute useless things";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        pythonEnv = pkgs.python311.withPackages (
+          ps: with ps; [
+            pip
+            setuptools
+            wheel
+          ]
+        );
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pythonEnv
+            pkgs.uv
+            pkgs.git
+          ];
+
+          shellHook = ''
+            if [ ! -d .venv ]; then
+              echo "creating virtualenv with uv..."
+              uv venv .venv
+            fi
+
+            export VIRTUAL_ENV="$PWD/.venv"
+            export PATH="$VIRTUAL_ENV/bin:$PATH"
+            export PIP_DISABLE_PIP_VERSION_CHECK=1
+
+            echo "# Python $(python --version 2>&1 | cut -d' ' -f2) | uv $(uv --version | cut -d' ' -f2)"
+          '';
+        };
+      }
+    );
+}
